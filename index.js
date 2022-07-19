7976566a8481154afdd0445dccb0f1e5c187b38a
ali-khalil-qbatch <109207474+ali-khalil-qbatch@users.noreply.github.com>
2022-07-19 11:20:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const pets = require("./pet");
 
-// console.log("My Pet name is \"" + pet.name + "\" and it a \"" + pet.type + "\" animal.");
-// console.log(pets);
-
 for (var pet in pets) {
-	// console.log(pet.name + " " + pet.type);
-	// console.log(pets[pet]);
 	var _pet = pets[pet];
 	console.log(_pet.title);
 }
+
+console.log("Info about all pets have been printed");
