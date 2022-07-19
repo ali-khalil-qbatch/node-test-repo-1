875f6514e45fe0ce1f52714d142e26e4180a21fa
ali-khalil-qbatch <ali.khalil@qbatch.com>
2022-07-19 11:13:48 +0500

    Commit 2

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const pets = require("./pet");
 // console.log("My Pet name is \"" + pet.name + "\" and it a \"" + pet.type + "\" animal.");
 // console.log(pets);
 
-// for (var pet in pets) {
-// 	// console.log(pet.name + " " + pet.type);
-// 	console.log(pets[pet]);
-// }
-
-pets.forEach(element => {
-	console.log(element);
-});
+for (var pet in pets) {
+	// console.log(pet.name + " " + pet.type);
+	// console.log(pets[pet]);
+	console.log(pet);
+}
